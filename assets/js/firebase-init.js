4dd2feea96577f341c37c8953eea21b071319ef8
farhorizonsest <Mhjerjawi836@gmail.com>
2026-01-17 06:10:53 +0300

    Force Deploy Firebase Core

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,22 @@
+// assets/js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATOnkG8yomjUFdKnNiPPJO24z0tF6e5wo",
+    authDomain: "mhstudios-836.firebaseapp.com",
+    projectId: "mhstudios-836",
+    storageBucket: "mhstudios-836.firebasestorage.app",
+    messagingSenderId: "172610798643",
+    appId: "1:172610798643:web:eaa7b218e8efa3aee64f46",
+    measurementId: "G-6EMB5KYCTK"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const dbID = "MH_STUDIOS_V1"; // Make sure this matches your Firestore path exactly!
